eaf74c4e5a845b089e6c5860c8553e529b4c41ae
julienhennet <julien.hennet@gmail.com>
2013-01-13 20:32:39 +0100

    basic game with deplacement and collisions

--- js/main.js
+++ js/main.js
@@ -1,104 +1,143 @@
-var KEYCODE_UP = 38; 	
-var KEYCODE_LEFT = 37; 	
-var KEYCODE_RIGHT = 39;
-var KEYCODE_DOWN = 40;
+/*!
+ * 
+ *   melonJS
+ *   http://www.melonjs.org
+ *		
+ *   Step by step game creation tutorial
+ *
+ **/
 
-var canvas;
-var stage;
-var screen_width;
-var screen_height;
-var background;
-var backgroundMask;
-var player;
-var contentManager;
-
-var down = false;
-
-
-function init() {
-    // on récupère l’instance du canvas puis on charge les images 
-    canvas = document.getElementById("testCanvas");
-
-    // création de l’objet Stage que l’on fait pointer vers notre canvas 
-    stage = new createjs.Stage(canvas);
-
-    // on récupère la largeur et la hauteur du canvas pour de futurs calculs savants 
-    screen_width = canvas.width;
-    screen_height = canvas.height;
-
-    contentManager = new ContentManager(stage, screen_width, screen_height);
-    //on indique que la fonction startGame est celle qu'on souhaite appeller a la fin du telechargement
-    contentManager.SetDownloadCompleted(startGame);
-    //on commence le téléchargement
-    contentManager.StartDownload();
-}
+// game resources
+var g_resources = [{
+    name: "tiles_test",
+    type: "image",
+    src: "data/area01_tileset/tiles_test.png"
+}, {
+    name: "area01",
+    type: "tmx",
+    src: "data/area01.tmx"
+}, {
+    name: "jelly_bean",
+    type: "image",
+    src: "data/sprite/jelly_bean.png"
+// the spinning coin spritesheet
+}, {
+    name: "spinning_coin_gold",
+    type: "image",
+    src: "data/sprite/spinning_coin_gold.png"
+// our enemty entity
+}];
 
 
-function startGame() {
-
-	background = new createjs.Bitmap(contentManager.imgBackground);
-	stage.addChild(background);
+var jsApp	= 
+{	
+	/* ---
 	
+		Initialize the jsApp
+		
+		---			*/
+	onload: function()
+	{
+		
+		// init the video
+		if (!me.video.init('jsapp', 640, 480, false, 1.0))
+		{
+			alert("Sorry but your browser does not support html 5 canvas.");
+         	return;
+		}
+				
+		// initialize the "audio"
+		me.audio.init("mp3,ogg");
+		
+		// set all resources to be loaded
+		me.loader.onload = this.loaded.bind(this);
+		
+		// set all resources to be loaded
+		me.loader.preload(g_resources);
 
-    // Notre héros peut être déplacé avec les flèches (gauche, droite) 
-    document.onkeydown = handleKeyDown;
-    document.onkeyup = handleKeyUp;
+		// load everything & display a loading screen
+		me.state.change(me.state.LOADING);
+	},
+	
+	
+	/* ---
+ 
+   callback when everything is loaded
+     
+   ---  */
+     
+	loaded: function ()
+	{
+	   // set the "Play/Ingame" Screen Object
+	   me.state.set(me.state.PLAY, new PlayScreen());
+	     
+	   // add our player entity in the entity pool
+	   me.entityPool.add("mainPlayer", PlayerEntity);
 
-    // On créé le héros
-    player = new Player(contentManager.imgPlayer, screen_width, screen_height);
-    player.x = 30;
-    player.y = 360;
+	   // add gold coins entity in the entity pool
+	   me.entityPool.add("CoinEntity", CoinEntity);
+	             
+	   // enable the keyboard
+	   me.input.bindKey(me.input.KEY.LEFT,  "left");
+	   me.input.bindKey(me.input.KEY.RIGHT, "right");
+	   me.input.bindKey(me.input.KEY.UP,  "up");
+	   me.input.bindKey(me.input.KEY.DOWN, "down");
 
-    stage.addChild(player);
-
-    // otherwise we could use Ticker.addListener(stage);
-	createjs.Ticker.addListener(window);
-    // Best Framerate targeted (60 FPS)
-	createjs.Ticker.useRAF = true;
-	createjs.Ticker.setFPS(60);
-}
-
-function tick() {
-
-	player.tick();
-	stage.update();
-}
-
-function handleKeyDown(e) {
-    //cross browser issues exist
-    if (!e) 
-    	{ var e = window.event; }
-    if(!down){
-	    switch (e.keyCode) {
-	        case KEYCODE_LEFT:
-            	player.gotoAndPlay("walkLeft");
-                player.directionX = -1;
-                player.directionY = 0;
-				break;
-			 case KEYCODE_RIGHT:
-            	player.gotoAndPlay("walkLeft_h");
-                player.directionX = 1;
-                player.directionY = 0;
-				break;
-	        case KEYCODE_DOWN:
-        		player.gotoAndPlay("walkDown");
-                player.directionX = 0;
-                player.directionY = 1;
-				break;
-	        case KEYCODE_UP:
-	            player.gotoAndPlay("walkUp");
-	            player.directionX = 0;
-	                player.directionY = -1;
-				break;
-
-	    }
-	    down = true;
+	   //see the boundingBox
+	   //me.debug.renderHitBox = true;
+	      
+	   // start the game
+	   me.state.change(me.state.PLAY);
 	}
-}
 
-function handleKeyUp(e) {
-	down = false;
-	player.stop();
-	player.directionX = 0;
-	player.directionY = 0;
-}
+}; // jsApp
+
+/* the in game stuff*/
+var PlayScreen = me.ScreenObject.extend(
+{
+
+   onResetEvent: function()
+	{	
+      // stuff to reset on state change
+    	me.levelDirector.loadLevel("area01");
+	},
+	
+	
+	/* ---
+	
+		 action to perform when game is finished (state change)
+		
+		---	*/
+	onDestroyEvent: function()
+	{
+	
+   }
+
+});
+
+
+/*----------------
+ a Coin entity
+------------------------ */
+var CoinEntity = me.CollectableEntity.extend({
+    // extending the init function is not mandatory
+    // unless you need to add some extra initialization
+    init: function(x, y, settings) {
+        // call the parent constructor
+        this.parent(x, y, settings);
+    },
+ 
+    // this function is called by the engine, when
+    // an object is touched by something (here collected)
+    onCollision: function() {
+        // do something when collected
+    }
+ 
+});
+
+
+//bootstrap :)
+window.onReady(function() 
+{
+	jsApp.onload();
+});
